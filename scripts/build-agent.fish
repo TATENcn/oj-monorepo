c684a36892c396cbab825d756dae6ea2e76cc404
xinhallow <xinhallow@outlook.com>
2026-06-06 15:26:22 +0800

    chore: 添加构建 agent 的开发用途脚本

---
+++ scripts/build-agent.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+# This script is for development purposes
+docker build services/judge-core --file ./services/judge-core/crates/agent/Dockerfile --tag judge-core:latest
+docker image save judge-core:latest -o judgecore.tar
+sudo ctr -n judge-core image import judgecore.tar
+rm judgecore.tar
