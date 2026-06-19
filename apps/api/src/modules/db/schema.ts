f63ea39ec3d22c12dc97c80b974d0d11c40db6a5
xinhallow <xinhallow@outlook.com>
2026-06-19 18:38:33 +0800

    feat: 添加 submission 的 schema

--- apps/api/src/modules/db/schema.ts
+++ apps/api/src/modules/db/schema.ts
@@ -1,12 +1,17 @@
 import { defineRelations } from "drizzle-orm";
 import type { BunSQLDatabase } from "drizzle-orm/bun-sql/postgres/driver";
-import { char, integer, pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { char, integer, jsonb, pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { Language, type VerdictResponse } from "models/judge-core";
 import { user } from "./auth.schema";
 
 export const difficultyEnumLiteral = ["入门", "普及-", "普及/提高-", "普及+/提高", "提高+/省选-", "省选/NOI-", "NOI/NOI+/CTSC"] as const;
 export const testCaseTypeEnumLiteral = ["example", "hidden"] as const;
+export const submissionStatusEnumLiteral = ["pending", "completed"] as const;
+export const acceptableLanguageEnumLiteral = [Language.Cpp] as const;
 export const difficultyEnum = pgEnum("difficulty", difficultyEnumLiteral);
 export const testCaseTypeEnum = pgEnum("test_case_type", testCaseTypeEnumLiteral);
+export const submissionStatusEnum = pgEnum("submission_status", submissionStatusEnumLiteral);
+export const acceptableLanguageEnum = pgEnum("acceptable_language", acceptableLanguageEnumLiteral);
 
 export const tags = pgTable("tags", {
 	id: uuid("id").primaryKey().defaultRandom(),
@@ -57,15 +62,32 @@ export const testCases = pgTable("test_cases", {
 	type: testCaseTypeEnum("type").notNull().default("hidden"),
 });
 
+export const submissions = pgTable("submissions", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	problemId: uuid("problem_id")
+		.notNull()
+		.references(() => problems.id),
+	userId: uuid("user_id")
+		.notNull()
+		.references(() => user.id),
+	sourceCode: text("source_code").notNull(),
+	status: submissionStatusEnum("submission_status").notNull().default("pending"),
+	result: jsonb("result").$type<VerdictResponse>(),
+	language: acceptableLanguageEnum("language").notNull(),
+	submittedAt: timestamp("submitted_at").notNull().defaultNow(),
+	completedAt: timestamp("completed_at"),
+});
+
 export const schema = {
 	tags,
 	problems,
 	problemTags,
 	testCases,
+	submissions,
 	user,
 };
 
-export const relations = defineRelations({ tags, problems, problemTags, testCases, user }, (r) => ({
+export const relations = defineRelations({ tags, problems, problemTags, testCases, submissions, user }, (r) => ({
 	problems: {
 		problemTags: r.many.problemTags({
 			from: r.problems.id,
@@ -78,6 +100,10 @@ export const relations = defineRelations({ tags, problems, problemTags, testCase
 		author: r.one.user({
 			from: r.problems.authorId,
 			to: r.user.id,
+		}),
+		submissions: r.many.submissions({
+			from: r.problems.id,
+			to: r.submissions.problemId,
 		}),
 	},
 	tags: {
@@ -102,10 +128,24 @@ export const relations = defineRelations({ tags, problems, problemTags, testCase
 			to: r.problems.id,
 		}),
 	},
+	submissions: {
+		problem: r.one.problems({
+			from: r.submissions.problemId,
+			to: r.problems.id,
+		}),
+		user: r.one.user({
+			from: r.submissions.userId,
+			to: r.user.id,
+		}),
+	},
 	user: {
 		problems: r.many.problems({
 			from: r.user.id,
 			to: r.problems.authorId,
+		}),
+		submissions: r.many.submissions({
+			from: r.user.id,
+			to: r.submissions.userId,
 		}),
 	},
 }));
