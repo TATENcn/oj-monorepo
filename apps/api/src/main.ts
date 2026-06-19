dd4cb2dd675e30acb6076cc79d8f2803ec9fc0c9
xinhallow <xinhallow@outlook.com>
2026-06-19 11:13:29 +0800

    feat: 添加 openapi 插件

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,8 @@
+import { openapi } from "@elysia/openapi";
 import { Elysia } from "elysia";
 import { authRoutePlugin } from "./modules/auth";
 
-new Elysia().use(authRoutePlugin).listen({ port: 3080 });
+new Elysia()
+	.use(authRoutePlugin)
+	.use(openapi({ provider: "scalar" }))
+	.listen({ port: 3080 });
