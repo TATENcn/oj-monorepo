826916c1dbc74500b203258397bb7b8793f0463f
xinhallow <xinhallow@outlook.com>
2026-06-19 18:26:47 +0800

    feat: 使 connect.ts 导出为一个 Elysia 插件

--- apps/api/src/modules/submission/connect.ts
+++ apps/api/src/modules/submission/connect.ts
@@ -1,3 +1,6 @@
+import Elysia from "elysia";
 import { initRabbitMq } from "utils";
 
-const { channel, config } = await initRabbitMq(process.env.RABBIT_MQ_URL!);
+const res = await initRabbitMq(process.env.RABBIT_MQ_URL!);
+
+export const mqPlugin = new Elysia({ name: "mq" }).decorate("mq", res);
