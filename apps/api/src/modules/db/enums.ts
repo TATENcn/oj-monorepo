1d1ae7a303d6d8f6d953be7d36fb762c0c36bf6d
xinhallow <xinhallow@outlook.com>
2026-06-21 15:33:37 +0800

    style: 使 enum literal 更紧凑

--- apps/api/src/modules/db/enums.ts
+++ apps/api/src/modules/db/enums.ts
@@ -1,11 +1,8 @@
 import { Language } from "models/judge-core";
 
 export const difficultyEnumLiteral = ["入门", "普及-", "普及/提高-", "普及+/提高", "提高+/省选-", "省选/NOI-", "NOI/NOI+/CTSC"] as const;
-
 export const testCaseTypeEnumLiteral = ["example", "hidden"] as const;
-
 export const submissionStatusEnumLiteral = ["pending", "completed"] as const;
-
 export const acceptableLanguageEnumLiteral = [Language.Cpp] as const;
 
 export type Difficulty = (typeof difficultyEnumLiteral)[number];
