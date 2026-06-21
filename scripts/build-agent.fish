4d396057eaaa0ffec02b58635dd3ccf98a950e9d
xinhallow <xinhallow@outlook.com>
2026-06-21 10:00:26 +0800

    fix: 修复 judge_core 子包名称错误问题

--- scripts/build-agent.fish
+++ scripts/build-agent.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 # This script is for development purposes
-docker build services/judge-core --file ./services/judge-core/Dockerfile.agent --tag judge-core:latest
+docker build services --file services/Dockerfile.agent --tag judge-core:latest
 docker image save judge-core:latest -o judgecore.tar
 sudo ctr -n judge-core image import judgecore.tar
 rm judgecore.tar
