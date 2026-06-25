45fb4cef2f9c24c5cf4b33a90cc08905569019cc
xinhallow <xinhallow@outlook.com>
2026-06-25 16:36:06 +0800

    feat: Supplement OpenAPI of `ContestController`

--- apps/api.v2/src/modules/contest/contest.controller.ts
+++ apps/api.v2/src/modules/contest/contest.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from "@nestjs/common";
+import { ApiAcceptedResponse, ApiCreatedResponse, ApiNoContentResponse, ApiOkResponse, ApiResponse } from "@nestjs/swagger";
 import type { User as AuthUser } from "better-auth";
 import { Auth, User } from "../auth/session.decorator";
 // biome-ignore lint/style/useImportType: Injection
@@ -22,23 +23,28 @@ export class ContestController {
 
 	@Post()
 	@Auth()
+	@HttpCode(HttpStatus.CREATED)
+	@ApiResponse({ status: 201, description: "Contest created", type: CreateContestResponse })
 	public async createContest(@Body() body: CreateContestRequest, @User() user: AuthUser): Promise<CreateContestResponse> {
 		const id = await this.repository.createContest(body, user.id);
 		return { id };
 	}
 
 	@Get()
+	@ApiOkResponse({ description: "List of contests", type: ContestResponse, isArray: true })
 	public async getContests(@Query() query: GetContestListParams): Promise<ContestResponse[]> {
 		return this.repository.getContests(query);
 	}
 
 	@Get(":id")
+	@ApiOkResponse({ description: "Contest details", type: ContestResponse })
 	public async getContest(@Param() params: GetContestParams): Promise<ContestResponse | null> {
 		return this.repository.getContest(params);
 	}
 
 	@Patch(":id")
 	@Auth()
+	@HttpCode(HttpStatus.ACCEPTED)
 	public async updateContestFields(@Param() params: UpdateContestFieldsParams, @Body() body: UpdateContestFieldsRequest, @User() user: AuthUser) {
 		await this.repository.updateContestFields(body, params, user.id);
 	}
