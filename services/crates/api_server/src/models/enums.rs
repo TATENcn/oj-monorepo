5ea8f6ee670d1e3f29d7d290d6eaa9bb1d98e085
xinhallow <xinhallow@outlook.com>
2026-06-28 16:50:31 +0800

    feat: Add difficulty variants

--- services/crates/api_server/src/models/enums.rs
+++ services/crates/api_server/src/models/enums.rs
@@ -1,9 +1,23 @@
 use sea_orm::{DeriveActiveEnum, EnumIter};
 
+/// REVIEW: Should we use this translation?
 #[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
 #[sea_orm(rs_type = "u64", db_type = "Integer")]
 pub enum Difficulty {
-    IdkHowToTranslate = 0,
+    /// 入门
+    A = 0,
+    /// 普及-
+    B = 1,
+    /// 普及/提高-
+    C = 2,
+    /// 普及+/提高
+    D = 3,
+    /// 提高+/省选-
+    E = 4,
+    /// 省选/NOI-
+    F = 5,
+    /// NOI/NOI+/CTSC
+    G = 6,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
