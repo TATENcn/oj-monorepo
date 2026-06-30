9add3c72f01bcd07b91bd2e7168f1c5e9700bb28
xinhallow <xinhallow@outlook.com>
2026-06-30 22:31:25 +0800

    feat: Add repositories to `ApiServerState`

--- services/crates/api_server/src/routers/mod.rs
+++ services/crates/api_server/src/routers/mod.rs
@@ -1,1 +1,10 @@
+use api_server_db::repositories::{problems::ProblemsRepo, test_cases::TestCasesRepo};
+use sea_orm::DatabaseConnection;
+
 pub mod problems;
+
+pub struct ApiServerState {
+    pub db: DatabaseConnection,
+    pub problems_repo: ProblemsRepo,
+    pub test_cases_repo: TestCasesRepo,
+}
